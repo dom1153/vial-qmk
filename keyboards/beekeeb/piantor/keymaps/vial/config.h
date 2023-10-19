f37d909c6517c0b6257289cb3155d9392a17decd
Dominic Dang <dtdd1153@live.com>
2023-10-19 05:35:39 -0500

    Adding piantor qmk and minor update mk47 planck

---
+++ keyboards/beekeeb/piantor/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* keyboard uid */
+#define VIAL_KEYBOARD_UID \
+    { 0x3C, 0x20, 0xFE, 0x88, 0xDF, 0x94, 0xFA, 0x36 }
+
+#define VIAL_UNLOCK_COMBO_ROWS \
+    { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS \
+    { 1, 4 }
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
